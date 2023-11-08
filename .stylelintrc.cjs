fa3d82f4a7310b165c24f3da754c18ad08d312d6
atatakobry <atatakobry@gmail.com>
2023-11-08 17:57:55 +0200

    add stylelint; `lint` --> `eslint`; re-fix all

---
+++ .stylelintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: ['stylelint-prettier'],
+  rules: {
+    'selector-class-pattern': [
+      '^[a-z][a-zA-Z0-9]+$',
+      {
+        message: (selector) => `Expected class selector "${selector}" to be snake-case`,
+      },
+    ],
+    'selector-id-pattern': [
+      '^[a-z][a-zA-Z0-9]+$',
+      {
+        message: (selector) => `Expected id selector "${selector}" to be snake-case`,
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+    'prettier/prettier': true,
+  },
+}
