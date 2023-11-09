fa8c490f887184b43077e48a36906d1b50e23a85
atatakobry <atatakobry@gmail.com>
2023-11-09 13:50:06 +0200

    update prettier config; re-fix all

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,17 @@ function App() {
           <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className={classNames(styles.logo, styles.react)}
-            alt="React logo"
-          />
+          <img src={reactLogo} className={classNames(styles.logo, styles.react)} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className={styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className={styles.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className={styles.readTheDocs}>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
