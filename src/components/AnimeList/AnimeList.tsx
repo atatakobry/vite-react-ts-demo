2be9c6df25ba6da6387551a20ca0fd0fd3efa6eb
atatakobry <atatakobry@gmail.com>
2023-11-13 22:24:28 +0200

    add anime type; add AnimeCard and AnimeList components

---
+++ src/components/AnimeList/AnimeList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AnimeCard from '../../components/AnimeCard';
+import { TAnime } from '../../types/anime';
+import styles from './AnimeList.module.scss';
+
+export type TAnimeListProps = {
+  animes: TAnime[];
+};
+
+const AnimeList: React.FC<TAnimeListProps> = ({ animes }) => (
+  <>
+    {animes.map((anime) => (
+      <div className={styles.animeCard} key={anime.id}>
+        <AnimeCard anime={anime} />
+      </div>
+    ))}
+  </>
+);
+
+export default AnimeList;
